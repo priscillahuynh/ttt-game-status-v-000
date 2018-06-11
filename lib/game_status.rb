28044a929f56b529094bcf4a31b64118deb726a1
Priscilla H <priscillamhuynh@gmail.com>
2018-06-11 14:13:25 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -56,7 +56,7 @@ end
 
 def winner(board)
 
-  if won(board) && win_combination[0]=="X"
+  if won?(board) && win_combination[0]=="X"
   return "X"
 elsif won(board) && win_combination[0]== "O"
   return "O"
