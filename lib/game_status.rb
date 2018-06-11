b317a171d96c22f731d3cb0cab2749ddea598bbf
Priscilla H <priscillamhuynh@gmail.com>
2018-06-11 10:22:30 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,7 +24,7 @@ def won?(board)
     return empty_board
   end
 WIN_COMBINATIONS.each |win_combo|
-  win_combo.each do {|value| value == "X"}
+  win_combo.any? {|value| value == "X","X","X"}
   # win_index_0 = WIN_COMBINATIONS[0]
   # win_index_1 = WIN_COMBINATIONS[1]
   # win_index_2 = WIN_COMBINATIONS[2]
