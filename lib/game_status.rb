7a8830273cb04eef3c5b41577782fe272c4cebe1
Priscilla H <priscillamhuynh@gmail.com>
2018-06-11 07:40:42 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,4 +27,10 @@ def won(board)
 
   position_1 = board[win_index_1]
   position_2 = board[win_index_2]
+  position_3 = board[win_index_3]
+  position_4 = board[win_index_4]
+  position_5 = board[win_index_5]
+  position_6 = board[win_index_6]
+  position_7 = board[win_index_7]
+  position_8 = board[win_index_8]
 end
