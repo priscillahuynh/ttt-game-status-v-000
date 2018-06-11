f1f7aeee523e3fe70c928d00f64b2c4c4e8caecd
Priscilla H <priscillamhuynh@gmail.com>
2018-06-11 08:41:01 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -37,6 +37,7 @@ WIN_COMBINATIONS.any? {|win_combo|
 if position_1== "X" && position_2 =="X" && position_3=="X"
   return win_combo
 elsif position_1 == "O" && position_2 =="O" && position_3 =="O"
+  return win combination
   # position_1 = board[win_index_1]
   # position_2 = board[win_index_2]
   # position_3 = board[win_index_3]
