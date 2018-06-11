afda9009ce60f95e745122004a5beb44fe2c5166
Priscilla H <priscillamhuynh@gmail.com>
2018-06-11 13:05:23 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -37,9 +37,8 @@ WIN_COMBINATIONS.each do |win_combination| # [0, 1, 2]
   #
   if position_1 == "X" && position_2 == "X" && position_3 == "X"
     return win_combination # return the win_combination indexes that won.
-  elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+  else position_1 == "O" && position_2 == "O" && position_3 == "O"
     return win_combination
-  else
   end
 end
 end
