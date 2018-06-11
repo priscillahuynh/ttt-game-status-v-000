dd624dc4179a12a844682d66fbb7804ef49eb2ad
Priscilla H <priscillamhuynh@gmail.com>
2018-06-11 07:38:56 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,5 +16,12 @@ WIN_COMBINATIONS = [
 ]
 
 def won
-  win_index_1 = WIN_COMBINATIONS[0]
+  win_index_1 = WIN_COMBINATIONS[0] 
+  win_index_2 = WIN_COMBINATIONS[1]
+  win_index_3 = WIN_COMBINATIONS[2]
+  win_index_4 = WIN_COMBINATIONS[3]
+  win_index_5 = WIN_COMBINATIONS[4] 
+  win_index_6 = WIN_COMBINATIONS[5]
+  win_index_7 = WIN_COMBINATIONS[6]
+  win_index_8 = WIN_COMBINATIONS[7]
 end
