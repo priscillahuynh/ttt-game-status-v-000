666fbee76ecde418fb503234fdb9e0233a6b7cc2
Priscilla H <priscillamhuynh@gmail.com>
2018-06-11 13:08:28 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -35,9 +35,7 @@ WIN_COMBINATIONS.each do |win_combination| # [0, 1, 2]
   # position_2 = board[win_index_2] # load the value of the board at win_index_2
   # position_3 = board[win_index_3] # load the value of the board at win_index_3
   #
-  if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return win_combination # return the win_combination indexes that won.
-  elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+  if position_1 == position_2 == position_3 == "X" 
     return win_combination
   end
 end
